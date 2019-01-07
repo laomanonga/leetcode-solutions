809b0ce30fa4ea1ebe6f16720fc69edc2f968b17
xyk <xyk>
2019-01-07 21:35:36 +0800

    'solution1-1'

--- src/Solution1.java
+++ src/Solution1.java
@@ -1,2 +1,20 @@
 public class Solution1 {
+
+    public static void main(String[] args) {
+        int[] result = twoSum(new int[]{2, 7, 11, 15}, 26);
+        System.err.println(result[0]);
+        System.err.println(result[1]);
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[]{0,0};
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
 }
