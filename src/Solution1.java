818364d1a08f3f8cb5f47995d573a777bc02aacc
xyk <xyk>
2019-01-08 20:54:47 +0800

    twosum1

--- src/Solution1.java
+++ src/Solution1.java
@@ -1,11 +1,12 @@
 public class Solution1 {
 
     public static void main(String[] args) {
-        int[] result = twoSum(new int[]{2, 7, 11, 15}, 26);
+        int[] result = twoSum1(new int[]{2, 7, 11, 15}, 17);
         System.err.println(result[0]);
         System.err.println(result[1]);
     }
-    public static int[] twoSum(int[] nums, int target) {
+
+    public static int[] twoSum1(int[] nums, int target) {
         int[] result = new int[]{0,0};
         for (int i = 0; i < nums.length; i++) {
             for (int j = i+1; j < nums.length; j++) {
