d09f06d42f9e09d3c14d9a01f7110e1fb17e0a53
xyk <xyk>
2019-01-11 21:08:02 +0800

    twosum2

--- src/Solution2.java
+++ src/Solution2.java
@@ -5,7 +5,7 @@ public class Solution2 {
 
 
     public static void main(String[] args) {
-        ListNode listNode1 = new ListNode(9);
+        ListNode listNode1 = new ListNode(2);
 //        listNode1.next.next = new ListNode(3);
 
         ListNode listNode2 = new ListNode(1);
